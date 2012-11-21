2d4a3abe83fdd54b9d06481f69b0b167f8a3524e
Anders Bryrup <andersbryrup@gmail.com>
2012-11-21 12:41:34 +0100

    timezone

--- web/index.php
+++ web/index.php
@@ -4,6 +4,8 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $loader = new \Composer\Autoload\ClassLoader();
 
+date_default_timezone_set('Europe/Copenhagen');
+
 $loader->add('Ishii', __DIR__.'/../src');
 $loader->add('Tobiassjosten\Silex\Provider', __DIR__.'/../vendor/tobiassjosten/facebook-service-provider/lib');
 
