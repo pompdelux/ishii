ae4eb8fea172a8f5a5ffa998bbacf1e1832dc85f
Anders Bryrup <andersbryrup@gmail.com>
2012-11-23 15:40:31 +0100

    changed the submit button

--- web/fx/js/main.js
+++ web/fx/js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
         }
     });
     // Facebook Me... :-)
-    $('#add-picture button.submit').click(function(e){
+    $('#submit-picture').click(function(e){
         e.preventDefault();
         $form = $('#add-picture');
         $this = $(this);
