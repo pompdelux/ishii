83621824b1d52055dadb4025c88aa7729f74ca6e
Anders Bryrup <andersbryrup@gmail.com>
2012-11-23 09:03:06 +0100

    stupid typo

--- web/fx/js/main.js
+++ web/fx/js/main.js
@@ -51,7 +51,7 @@ $(document).ready(function () {
     $('#add-picture #form_description').keyup(function(e){
         $('#preview p').text($(this).val());
     });
-    if(jQuery.isFunction(jQuery.fn.filereader)){
+    if(jQuery.isFunction(jQuery.fn.fileReader)){
         $('#form_picture').fileReader({
             id : 'fileReaderSWFObject',
             filereader : '/fx/js/vendor/filereader/filereader.swf',
