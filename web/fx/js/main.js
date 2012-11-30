5980c0bb5b9db04df9dca1709cd5ffb5231afff7
Anders Bryrup <andersbryrup@gmail.com>
2012-11-30 01:34:11 -0800

    IE fix within iframe

--- web/fx/js/main.js
+++ web/fx/js/main.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
                 //console.log('Welcome!  Fetching your information.... ');
                 FB.api('/me', function(response) {
                     //console.log('Good to see you, ' + response.name + '.');
-                    location.href = $this.attr('href');
+                    self.location.href = $this.attr('href');
                 });
             } else {
                 //console.log('User cancelled login or did not fully authorize.');
